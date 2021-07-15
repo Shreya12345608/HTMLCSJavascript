1724e165df227ca8bd5f4158422a928cafa7ac51
Shreya Malviya <malviyashreya26@gmail.com>
2021-07-15 18:42:40 +0530

    [Shreya] Add: Validate name

--- js/form.js
+++ js/form.js
@@ -1,6 +1,28 @@
+//making range button synchronised with values of user
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
+//in range button the output shown should always be equal to value the user is updating
 output.textContent = salary.value;
-salary.addEventListener('input', function () {
+salary.addEventListener('input', function () 
+{
     output.textContent = salary.value;
-})
+});
+//uc1 -name starts with capital letter and min 3 letter
+const text = document.querySelector('#text');
+
+const textError = document.querySelector('.text-error');
+//this will throw an event showing that name is incorrect or nothing
+
+text.addEventListener('input', function () 
+{
+    //regex for Name
+    let nameRegex = RegExp('^[A-Z]{1}[A-Za-z]{2,}$');
+    if (nameRegex.test(text.value)) 
+    {
+        textError.textContent = "";
+    }
+    else 
+    {
+        textError.textContent = "Name is incorrect";
+    }
+});
